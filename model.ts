bf737987ee7d36b75909b4f9c8f0f4f4e0daac16
welchi <welchizm@gmail.com>
2018-12-19 15:34:03 +0900

    Change threshold

--- model.ts
+++ model.ts
@@ -83,7 +83,7 @@ tf.loadModel("http://localhost:5000/model.json").then(model => {
           return;
         }
         console.log(punch.toFixed(2), kick.toFixed(2));
-        if (kick > punch && kick >= 0.5) {
+        if (kick > punch && kick >= 0.35) {
           console.log(
             "%cKick: " + kick.toFixed(2),
             "color: red; font-size: 30px"
@@ -91,7 +91,7 @@ tf.loadModel("http://localhost:5000/model.json").then(model => {
           connection.send("Kick");
           return;
         }
-        if (punch > kick && punch >= 0.5) {
+        if (punch > kick && punch >= 0.35) {
           console.log(
             "%cPunch: " + punch.toFixed(2),
             "color: blue; font-size: 30px"
